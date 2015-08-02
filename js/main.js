f025ff18d982b3d46fa26be26ff7e449529613d8
Benji Wilson <benwilson@wilsonben.co.uk>
2015-08-02 18:20:19 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+console.log('it works');
+
+
+
+//$(function(){
+  //$(".classloader").click(function(){
+  	//console.log('click')
+    //$("#content").load("content2.html");
+  //});
+//});
+
+var myurl = 'text.html';
+
+$('button').click(function(){
+    $('div.loadme').load(myurl);
+});
+
+
+
+
+
+
+
+
+
+
+// in text.html open form
+
+// have one initial input field
+
+// create button to add another person
+
+// push the .val() into people array
+
+// set .val('')
+
+// interate over people array and append html of names
+
+// who's round? button then selects random name form array
+
+// name is appended to html
+
+
